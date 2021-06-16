fdab4bba30e177c481d0c763d7175391805284d1
cheikh <cheikh.kebe.01@gmail.com>
2021-06-16 10:26:37 +0200

    validates fixed to seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   #validations 
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  validates :summoner_name, uniqueness: true, optional: true
-  validates :summoner_name, presence: true, on: :update
+  validates :summoner_name, uniqueness: true, presence: true, on: :update
   
 end
