ecac2e17e617e450bc88b2af4a82fe8a4432c357
Florentin437 <hiphopottam@gmail.com>
2021-06-18 16:23:29 +0200

    fix bug

---
+++ app/assets/javascripts/icons_active 3.js
@@ -0,0 +1,21 @@
+//*** ADD ACTIVE CLASS TO CONTAINER-ICON ONCLICK  ***//
+
+function iconsActive() {
+
+    var homeIcon = document.getElementById('home-add');
+    var arrowIcon = document.getElementById('arrow-add');
+    var messageIcon = document.getElementById('message-add');
+    var categoryIcon = document.getElementById('category-add');
+    var discoveryIcon = document.getElementById('discovery-add');
+
+    console.log(homeIcon);
+    console.log(arrowIcon);
+    console.log(messageIcon);
+    console.log(categoryIcon);
+    console.log(discoveryIcon);
+
+    
+
+}
+
+iconsActive()
