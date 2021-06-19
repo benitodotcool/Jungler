be9a157a9d7cfb37b8a1678d4dd9ac6bbb579be6
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-06-19 19:34:18 +0200

    deconnection_button in users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
