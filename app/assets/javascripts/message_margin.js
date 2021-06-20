ee034c114ea65688eed20b05ed3d06ef3475c08e
Valentin Bousquet <bousquet.valentin@gmail.com>
2021-06-20 15:11:23 +0200

    conversations right finish

--- app/assets/javascripts/message_margin.js
+++ app/assets/javascripts/message_margin.js
@@ -2,15 +2,17 @@ function messageMargin() {
 
     let allMessages = document.querySelectorAll('.message');
 
-    allMessages.forEach((message) => {
+    allMessages.forEach((message) => {        
 
-        let nextMessage = message.nextSibling;
-
-        if message.classList.contains('message-send') && nextMessage.classList.contains('message-received'){
+        if (message.classList.contains('message-send') && message.nextElementSibling.classList.contains('message-received')) {
             message.classList.add('message-margin');
         }
-    });
 
+        if (message.classList.contains('message-received') && message.nextElementSibling.classList.contains('message-send')) {
+            message.classList.add('message-margin');
+        }
+
+    });
 
 }
 
