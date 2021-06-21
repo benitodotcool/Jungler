233eefa8bcfca11df312c864a51fa29def2ac1f4
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-06-21 10:17:50 +0200

    users edit after signup/signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   
   def after_sign_in_path_for(user)
     stored_location_for(user) 
-    edit_user_path(params[:id])
+    edit_user_path(current_user.id)
   end
 end
