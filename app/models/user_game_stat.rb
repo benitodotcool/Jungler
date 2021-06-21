c561c2be2b69404913d592bb1bd27606540702f8
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-06-21 19:33:04 +0200

    routes modify

--- app/models/user_game_stat.rb
+++ app/models/user_game_stat.rb
@@ -4,8 +4,8 @@ belongs_to :user, dependent: :destroy
 
 validates :level, presence: true, numericality: { greater_than: 0, less_than: 3100 }, on: :update
 validates :summoner_id,length: {maximum: 63}, on: :update
-validates :primary_role, presence: true, on: :update
-validates :secondary_role, presence: true, on: :update
-validates :description, presence: true, length: {minimum: 30, maximum: 280}, on: :update
+#validates :primary_role, presence: true, on: :update
+#validates :secondary_role, presence: true, on: :update
+#validates :description, presence: true, length: {minimum: 30, maximum: 280}, on: :update
 
 end
