90ad7ee4709001e238c6055898607f33239d273d
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-06-21 09:35:03 +0200

    test user_edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
+  
+  
   private
 
-  # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
     root_path
+  end
+  
+  def after_sign_in_path_for(user)
+    stored_location_for(user) 
+    edit_user_path(params[:id])
   end
 end
