25a894f81c03038b5ce095b63ab2cde5b30d3487
Florentin437 <hiphopottam@gmail.com>
2021-06-23 21:08:22 +0200

    error_pages

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   root 'landing_page#index'
+
+  get '/404' , to: 'errors#not_found'
+  get '/422' , to: 'errors#unacceptable'
+  get '/500' , to: 'errors#server_error'
 end
