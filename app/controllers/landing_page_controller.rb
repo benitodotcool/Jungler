e82c48fa941e0ec1cadaa422a48873e5069c9fff
Valentin Bousquet <bousquet.valentin@gmail.com>
2021-06-23 22:46:40 +0200

    All goods

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -4,8 +4,10 @@ class LandingPageController < ApplicationController
     @all_matchs      = Conversation.all.count
     @all_messages    = Message.all.count
     @user            = User.new
-    
+    begin
     @user_game_stat = UserGameStat.find_by(user_id:current_user.id)
+    rescue
+    end
   end
 
   def show
