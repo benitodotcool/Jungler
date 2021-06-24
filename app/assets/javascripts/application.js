499449bddfa8dab3c6844b43d11a95ffa8a020a5
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-06-24 11:59:55 +0200

    fix change tag_list to html redirect, clean js.files useless

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require activestorage
 //= require_tree .
 //= require particules
@@ -18,4 +17,3 @@
 //= require nav_action
 //= require message_margin
 //= require conversation_name
-//= require change_color
