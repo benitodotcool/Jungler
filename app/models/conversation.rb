cfa890f09571ef72769fff1305adf0b030c3068a
Valentin Bousquet <bousquet.valentin@gmail.com>
2021-06-27 21:47:40 +0200

    Cleaning models spaces

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,9 +1,7 @@
 class Conversation < ApplicationRecord
 
-
   belongs_to :participant_a, class_name: 'User'
   belongs_to :participant_b, class_name: 'User'
-
 
   validates :participant_a, presence: true
   validates :participant_b, presence: true
@@ -14,14 +12,12 @@ class Conversation < ApplicationRecord
   after_create :conversation_send_to_b
 
   def conversation_send_to_a
-
     @user_id_a = Conversation.find(self.id).participant_a_id
     @user_a = User.find(@user_id_a)
     UserMailer.conversation_email_to_a(@user_a).deliver_now
   end
 
   def conversation_send_to_b
-
     @user_id_b = Conversation.find(self.id).participant_b_id
     @user_b = User.find(@user_id_b)
     UserMailer.conversation_email_to_b(@user_b).deliver_now
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,5 @@ class User < ApplicationRecord
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
-
   
 end
